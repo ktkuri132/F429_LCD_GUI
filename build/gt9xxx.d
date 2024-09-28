6400d50c72c7c16502eabb98dd2bf921603407c1
m <3457532883@qq.com>
2024-09-28 16:58:38 +0800

    main

---
+++ build/gt9xxx.d
@@ -0,0 +1,22 @@
+build/gt9xxx.o: TOUCH/gt9xxx.c LCD/lcd.h system/sys/sys.h \
+ User/stm32f4xx.h core/core_cm4.h core/core_cmInstr.h core/core_cmFunc.h \
+ core/core_cmSimd.h User/system_stm32f4xx.h USER_LIB/inc/lib.h \
+ USER_LIB/inc/gpio.h system/delay/delay.h system/usart/usart.h \
+ SDRAM/sdram.h LCD/ltdc.h TOUCH/gt9xxx.h TOUCH/ctiic.h TOUCH/gt9xxx.h
+LCD/lcd.h:
+system/sys/sys.h:
+User/stm32f4xx.h:
+core/core_cm4.h:
+core/core_cmInstr.h:
+core/core_cmFunc.h:
+core/core_cmSimd.h:
+User/system_stm32f4xx.h:
+USER_LIB/inc/lib.h:
+USER_LIB/inc/gpio.h:
+system/delay/delay.h:
+system/usart/usart.h:
+SDRAM/sdram.h:
+LCD/ltdc.h:
+TOUCH/gt9xxx.h:
+TOUCH/ctiic.h:
+TOUCH/gt9xxx.h:
