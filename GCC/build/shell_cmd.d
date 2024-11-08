f3356e146172570cff30ad5e6b7c4ba5b5a94426
M <3457532883@qq.com>
2024-11-08 09:24:19 +0000

    忽略build

--- GCC/build/shell_cmd.d
+++
@@ -1,1 +0,0 @@
-GCC/build/shell_cmd.o: Lib/src/shell_cmd.cpp
--- GCC/build/startup_stm32f429_439xx.d
+++
@@ -1,2 +0,0 @@
-GCC/build/startup_stm32f429_439xx.o: \
- Libraries/CMSIS/Device/ST/STM32F4xx/Source/Templates/gcc_ride7/startup_stm32f429_439xx.s
